da13aa740d357f58829457d9fe7fcb17241bcbdf
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-09-29 23:34:26 +0600

    adding action button at the last column

--- resources/views/admin/student/list.blade.php
+++ resources/views/admin/student/list.blade.php
@@ -8,52 +8,59 @@
                 <table class="table hover data-table-export nowrap">
                     <thead>
                         {{-- todo add asc and dec toggleable icon  --}}
-                      <tr>
-                        <th>#</th>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Contact Number</th>
-                        <th>Guardian Nubmer</th>
-                        <th>Seat Number</th>
-                        <th>Status</th>
-                        <th>Address</th>
-                        <th>Remarks</th>
-                        <th>Action</th>
-                      </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Department</th>
+                            <th>Contact Number</th>
+                            <th>Guardian Nubmer</th>
+                            <th>Seat Number</th>
+                            <th>Status</th>
+                            <th>Address</th>
+                            <th>Remarks</th>
+                            <th>Action</th>
+                        </tr>
                     </thead>
                     <tbody>
                         @foreach ($students as $student)
-                        @if ($student->status==1)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $student->studentID }}</td>
-                            <td>{{ $student->name }}</td>
-                            <td>{{ $student->dept }}</td>
-                            <td>{{ $student->phone }}</td>
-                            <td>{{ $student->g_phone }}</td>
-                            <td>{{ $student->seat_id }}</td>
-                            <td>{{ $student->status }}</td>
-                            <td>{{ $student->current_LT }}</td>
-                            <td>{{ $student->remarks }}</td>
-                            <td>
-                                {{-- todo add functionality in these buttons --}}
-                                <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip" data-placement="bottom" title="download-student-info"><i class="icon-copy dw dw-download"></i></a>
-{{--{{ route('student-information.download',$student->id) }} --}}
-                                <a href="#" class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="bottom" title="View"><i class="icon-copy fi-torsos-female-male"></i></a>
-                                {{-- {{route('student-information.show', $student->id)}} --}}
-                                <a href="#" class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="bottom" title="Edit"><i class="icon-copy dw dw-edit-1"></i></a>
-                                {{-- {{route('student-information.edit', $student->id)}} --}}
-                                <a href="#" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="icon-copy dw dw-trash1"></i></a>
-                                {{-- {{ route('student-information.destroy',$student->id)}} --}}
-                            </td>
-                        </tr>
-                        @endif
+                            @if ($student->status == 1)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $student->studentID }}</td>
+                                    <td>{{ $student->name }}</td>
+                                    <td>{{ $student->dept }}</td>
+                                    <td>{{ $student->phone }}</td>
+                                    <td>{{ $student->g_phone }}</td>
+                                    <td>{{ $student->seat_id }}</td>
+                                    <td>{{ $student->status }}</td>
+                                    <td>{{ $student->current_LT }}</td>
+                                    <td>{{ $student->remarks }}</td>
+                                    <td>
+                                        {{-- todo add functionality in these buttons --}}
+                                        <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip"
+                                            data-placement="bottom" title="download-student-info"><i
+                                                class="icon-copy dw dw-download"></i></a>
+                                        {{-- {{ route('student-information.download',$student->id) }} --}}
+                                        <a href="#" class="btn btn-success btn-sm" data-toggle="tooltip"
+                                            data-placement="bottom" title="View"><i
+                                                class="icon-copy fi-torsos-female-male"></i></a>
+                                        {{-- {{route('student-information.show', $student->id)}} --}}
+                                        <a href="#" class="btn btn-warning btn-sm" data-toggle="tooltip"
+                                            data-placement="bottom" title="Edit"><i
+                                                class="icon-copy dw dw-edit-1"></i></a>
+                                        {{-- {{route('student-information.edit', $student->id)}} --}}
+                                        <a href="#" class="btn btn-danger btn-sm" data-toggle="tooltip"
+                                            data-placement="bottom" title="Delete"><i
+                                                class="icon-copy dw dw-trash1"></i></a>
+                                        {{-- {{ route('student-information.destroy',$student->id)}} --}}
+                                    </td>
+                                </tr>
+                            @endif
                         @endforeach
                     </tbody>
-                  </table>
+                </table>
             </div>
         </div>
     </div>
 @endsection
-
