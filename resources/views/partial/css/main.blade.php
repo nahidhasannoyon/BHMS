815a460ee32edcbdb8e704da5d75bbb9f1b16abe
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-09-30 17:15:05 +0600

    configuring for sweet alert .

--- resources/views/partial/css/main.blade.php
+++ resources/views/partial/css/main.blade.php
@@ -1,36 +1,13 @@
-		<link
-			rel="apple-touch-icon"
-			sizes="180x180"
-			href="{{ asset('vendors/images/apple-touch-icon.png') }}"
-		/>
-		<link
-			rel="icon"
-			type="image/png"
-			sizes="32x32"
-			href="{{ asset('vendors/images/favicon-32x32.png') }}"
-		/>
-		<link
-			rel="icon"
-			type="image/png"
-			sizes="16x16"
-			href="{{ asset('vendors/images/favicon-16x16.png') }}"
-		/>
-        <link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/core.css') }}" />
-		<link
-			rel="stylesheet"
-			type="text/css"
-			href="{{ asset('vendors/styles/icon-font.min.css') }}"
-		/>
-		<link
-			rel="stylesheet"
-			type="text/css"
-			href="{{ asset('src/plugins/datatables/css/dataTables.bootstrap4.min.css') }}"
-		/>
-		<link
-			rel="stylesheet"
-			type="text/css"
-			href="{{ asset('src/plugins/datatables/css/responsive.bootstrap4.min.css') }}"
-		/>
-		<link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/style.css') }}" />
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendors/images/apple-touch-icon.png') }}" />
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('vendors/images/favicon-32x32.png') }}" />
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('vendors/images/favicon-16x16.png') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/core.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/icon-font.min.css') }}" />
+  <link rel="stylesheet" type="text/css"
+  href="{{ asset('src/plugins/datatables/css/dataTables.bootstrap4.min.css') }}" />
+  <link rel="stylesheet" type="text/css"
+  href="{{ asset('src/plugins/datatables/css/responsive.bootstrap4.min.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/style.css') }}" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
 
-        @stack('css')
+  @stack('css')
