99d6b36c405af019a8789ad7363d11dcca0f91d5
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-02 00:43:25 +0600

    adding try catch and surety message of monthly bill creation

--- app/Http/Controllers/MonthlyBillController.php
+++ app/Http/Controllers/MonthlyBillController.php
@@ -14,11 +14,16 @@ class MonthlyBillController extends Controller
     }
     public function store(Request $request)
     {
-        $monthly_bill = new MonthlyBill();
-        $monthly_bill->studentID = $request->studentID;
-        $monthly_bill->amount = $request->amount;
-        $monthly_bill->date = $request->date;
-        $monthly_bill->save();
-        return redirect()->route('monthly-bill');
+        try {
+            $monthly_bill = new MonthlyBill();
+            $monthly_bill->studentID = $request->studentID;
+            $monthly_bill->amount = $request->amount;
+            $monthly_bill->date = $request->date;
+            $monthly_bill->save();
+            toast('New Monthly Bill added.', 'success');
+            return redirect()->back();
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
 }
