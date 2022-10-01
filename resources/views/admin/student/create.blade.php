ab5ab6431526c1016b713abd97fd31377a5e11ab
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-02 00:51:29 +0600

    making some input fields required and adding required sign, adding level term and removing address input field, showing the actual hostel seat info to allocate it to student, ,

--- resources/views/admin/student/create.blade.php
+++ resources/views/admin/student/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="pd-20 card-box mb-30">
         <div class="text-center">
-            <h4 class="text-blue" style="padding-bottom: 10px"><u>Add Student</u><span class="text-danger">*</span></h4>
+            <h4 class="text-blue" style="padding-bottom: 10px"><u>Add Student</u></h4>
         </div>
         {{-- {{ route('admin.student.store') }} --}}
         <div class="col-md-12">
@@ -11,63 +11,82 @@
                 <div class="col-md-12">
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label>Student Name:</label>
-                            <input class="form-control" type="text" name="name" placeholder="Name">
+                            <label>Student Name:</label><span class="text-danger">*</span>
+                            <input class="form-control" type="text" name="name" placeholder="Name" required
+                                value="{{ old('name') }}">
                         </div>
                         <div class="form-group col-md-6">
-                            <label>Student ID:</label>
-                            <input type="text" class="form-control" name="studentID" data-validation="required" value="">
+                            <label>Student ID:</label><span class="text-danger">*</span>
+                            <input type="text" class="form-control" name="studentID" data-validation="required"
+                                value="{{ old('studentID') }}" required>
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="">Department:</label>
-                            <select name="dept" class="custom-select2 form-control" style="width: 100%; height: 38px;" data-validation="required">
-                                <option selected>Choose...</option>
-                                <option value="cse">CSE</option>
-                                <option value="eee">EEE</option>
-                                <option value="ce">CE</option>
-                                <option value="bba">BBA</option>
-                                <option value="engh">English</option>
-                                <option value="llb">LLB</option>
+                            <label for="">Department:</label><span class="text-danger">*</span>
+                            <select name="dept" class="custom-select2 form-control" style="width: 100%; height: 38px;"
+                                data-validation="required" required>
+                                <option selected disabled value>Choose...</option>
+                                <option value="CSE">CSE</option>
+                                <option value="EEE">EEE</option>
+                                <option value="CE">CE</option>
+                                <option value="BBA">BBA</option>
+                                <option value="English">English</option>
+                                <option value="LLB">LLB</option>
                             </select>
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="">Seat Number:</label>
-                            <select class="custom-select2 form-control" style="width: 100%; height: 38px;" name="seat_id" data-validation="required" placeholder='Select a Value'>
-                                <option selected>Choose...</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                
+                            <label for="">Level-Term:</label><span class="text-danger">*</span>
+                            <select class="custom-select2 form-control" style="width: 100%; height: 38px;" name="current_LT"
+                                data-validation="required" placeholder='Select a Value' required>
+                                <option selected disabled value>Choose...</option>
+                                <option value="1-1">1-1</option>
+                                <option value="1-2">1-2</option>
+                                <option value="2-1">2-1</option>
+                                <option value="2-2">2-2</option>
+                                <option value="3-1">3-1</option>
+                                <option value="3-2">3-2</option>
+                                <option value="4-1">4-1</option>
+                                <option value="4-2">4-2</option>
+
                             </select>
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="">Student Phone Number:</label>
-                            <input type="text" class="form-control" name="phone" data-validation="required" value="">
+                            <label for="">Seat Number:</label><span class="text-danger">*</span>
+                            <select class="custom-select2 form-control" style="width: 100%; height: 38px;" name="seat_id"
+                                data-validation="required" placeholder='Select a Value' required>
+                                <option selected disabled value>Choose...</option>
+                                @foreach ($hostels as $hostel)
+                                    <option value="{{ $hostel->id }}">
+                                        {{ $hostel->building_name . '-' . $hostel->floor . '-' . $hostel->flat . '-' . $hostel->seat }}
+                                    </option>
+                                @endforeach
+
+                            </select>
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="">Guardian Phone Number:</label>
-                            <input type="text" class="form-control" name="g_phone" data-validation="required" value="">
+                            <label for="">Student Phone Number:</label><span class="text-danger">*</span>
+                            <input type="text" class="form-control" name="phone" data-validation="required"
+                                value="" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="">Guardian Phone Number:</label><span class="text-danger">*</span>
+                            <input type="text" class="form-control" name="g_phone" data-validation="required"
+                                value="" required>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="">Remarks:</label>
-                            <input type="text" class="form-control" name="remarks" data-validation="required" value="">
+                            <input type="text" class="form-control" name="remarks" data-validation="required"
+                                value="">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="">Status:</label>
-                            <input type="text" value="Active" class="form-control text-success" name="status" data-validation="required">
+                            <label for="">Status:</label><span class="text-danger">*</span>
+                            <input type="text" value="Active" class="form-control text-success" name="status"
+                                data-validation="required" required>
                         </div>
                         <div class="form-group col-md-12">
-                            <label for="">Address:</label>
-                            <textarea class="form-control" name="current_LT" id="" cols="30" rows="5" data-validation="required"></textarea>
+                            <button class="btn btn-success btn-md float-right " type="submit"><i
+                                    class="icon-copy dw dw-up-arrow-11"></i>Submit</button>
                         </div>
-                        <div class="form-group col-md-12">
-                            <button class="btn btn-success btn-md float-right " type="submit"><i class="icon-copy dw dw-up-arrow-11"></i>Submit</button>
-                        </div>
-                
-                </div>
-                
+                    </div>
             </form>
         </div>
     </div>
