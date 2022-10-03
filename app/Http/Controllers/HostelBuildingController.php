4cf5d12b9e60d0570a5ec2c572c76263a67edfa3
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-03 17:40:33 +0600

    adding new controller to show and store data of new hostel building.

---
+++ app/Http/Controllers/HostelBuildingController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HostelBuilding;
+use App\Http\Requests\StoreHostelBuildingRequest;
+use App\Http\Requests\UpdateHostelBuildingRequest;
+use Illuminate\Http\Request;
+
+class HostelBuildingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $hostelBuildings = HostelBuilding::all();
+        return view('admin.hostel.building_list', compact('hostelBuildings'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreHostelBuildingRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $hostelBuilding = new HostelBuilding();
+        $hostelBuilding->name = $request->name;
+        $hostelBuilding->save();
+        toast('New Building Added.', 'success');
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HostelBuilding  $hostelBuilding
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HostelBuilding $hostelBuilding)
+    {
+        return $hostelBuilding;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HostelBuilding  $hostelBuilding
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HostelBuilding $hostelBuilding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateHostelBuildingRequest  $request
+     * @param  \App\Models\HostelBuilding  $hostelBuilding
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateHostelBuildingRequest $request, HostelBuilding $hostelBuilding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HostelBuilding  $hostelBuilding
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HostelBuilding $hostelBuilding)
+    {
+        //
+    }
+}
