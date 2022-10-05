dd2c27a34c458667d4e65f24af09b627f53070af
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-05 22:08:00 +0600

    adding login page and controller.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Policies\StudentPolicy;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -36,5 +41,35 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showStudentLoginForm()
+    {
+        return view('student.auth.login');
+    }
+    public function studentLogin(Request $request)
+    {
+        $studentID = $request->studentID;
+        $enteredPassword = $request->password;
+        $enteredPassword = Hash::make($enteredPassword);
+        $studentCheck = Student::where('studentID', $studentID)->exists();
+        if ($studentCheck) {
+            $existingPassword = Student::select('password')->where('studentID', $studentID)->first();
+            return $existingPassword;
+            $existingPassword = $existingPassword->password;
+            $check = Hash::check($enteredPassword, $existingPassword);
+            return $enteredPassword . ' == ' .  $existingPassword .  $check;
+            if ($check) {
+                Session::put('studentID', $studentID);
+                // return redirect()->route('student-home');
+                return "logged in successfully";
+            } else {
+                toast('Invalid Student ID/Password', 'error');
+                return 'Invalid Student ID/Password';
+            }
+        } else {
+            toast('No Records Found', 'error');
+            return "something went wrong";
+        }
     }
 }
