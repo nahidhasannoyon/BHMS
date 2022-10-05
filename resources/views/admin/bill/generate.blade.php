31edeef7c0621d34c55ae942cdb6e28f4daceb5e
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-05 22:04:07 +0600

    adding dropdown to select bill type and student id, date and amount

---
+++ resources/views/admin/bill/generate.blade.php
@@ -0,0 +1,46 @@
+@extends('layout.master')
+@stack('css')
+
+@section('content')
+    <div class="pd-20 card-box mb-30">
+        <div class="text-center">
+            <h4 class="text-blue" style="padding-bottom: 10px"><u>Add Student Bill</u></h4>
+        </div>
+        {{-- {{ route('admin.student.store') }} --}}
+        <div class="col-md-12">
+            <form action="#" method="POST">
+                @csrf
+                <div class="col-md-12">
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="">Choose the type of bill:</label><span class="text-danger">*</span>
+                            <select name="typesOfBill" class="custom-select2 form-control typesOfBill"
+                                style="width: 100%; height: 38px;" data-validation="required" required>
+                                <option selected disabled value>Choose...</option>
+
+
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 inputs-container">
+                    <div class="form-row inputs">
+                        <div class=" form-group col-md-4">
+                            <input type="text" class="form-control" name="studentID" data - validation="required"
+                                value="" required placeholder="Student ID">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <input type="date" class="form-control" name="date"
+                                min="{{ Carbon\Carbon::now()->format('Y-m-d') }}"
+                                value="{{ Carbon\Carbon::now()->format('Y-m-d') }}">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <input type="number" class="form-control" name="amount" data - validation="required"
+                                value="" required placeholder="Amount">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
