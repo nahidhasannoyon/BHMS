2a3fc3ded10dbb2286ca583527b5a90f54db381e
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-11 14:19:52 +0600

    moving the modal at the bottom and making page responsive.

--- resources/views/admin/bill/types.blade.php
+++ resources/views/admin/bill/types.blade.php
@@ -8,56 +8,12 @@
                     data-target="#add-meal-type">
                     <i class="icon-copy dw dw-add" style="font-family: dropways, Bangla526, sans-serif;"></i>
                     Add Bills</a>
-                <div class="modal fade bs-example-modal-lg" id="add-meal-type" tabindex="-1" role="dialog"
-                    aria-hidden="true">
-                    <div class="modal-dialog modal-lg modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title" id="myLargeModalLabel">Add New Bill Type From Here:</h4>
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <form action="{{ route('types-of-bill') }}" method="post">
-                                            @csrf
-                                            <div class="form-row">
-                                                <div class="form-group col-md-12">
-                                                    <label for="">Name:</label>
-                                                    <input type="text" class="form-control" name="name" required>
-                                                </div>
-                                                <div class="form-group col-md-12">
-                                                    <label for="">Status:</label>
-                                                    <select name="status" class="custom-select2 form-control"
-                                                        style="width: 100%; height: 38px;" data-validation="required"
-                                                        required>
-                                                        <option selected disabled value>Choose...</option>
-                                                        <option value="active">Active</option>
-                                                        <option value="inactive">Inactive</option>
-
-                                                    </select>
-                                                </div>
-                                                <div class="form-group col-md-12">
-                                                    <button type="submit" class="btn btn-success btn-md float-right">
-                                                        <i class="icon-copy dw dw-add"
-                                                            style="font-family: dropways, Bangla526, sans-serif;"></i> Add
-                                                        Bill Type
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="row align-items-center">
-            <div class="col-md-12">
+            <div class="col-md-12 table-reponsive">
                 <h3 class="text-info text-center pd-10"><u>Types of Bill</u></h3>
-                <table class="table hover data-table-export nowrap">
+                <table class="table table-striped">
                     <thead>
                         <tr>
                             {{--  todo add asc and dec icon to sort --}}
@@ -92,6 +48,48 @@
                         @endforeach
                     </tbody>
                 </table>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade bs-example-modal-lg" id="add-meal-type" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myLargeModalLabel">Add New Bill Type From Here:</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <form action="{{ route('types-of-bill') }}" method="post">
+                                @csrf
+                                <div class="form-row">
+                                    <div class="form-group col-md-12">
+                                        <label for="">Name:</label>
+                                        <input type="text" class="form-control" name="name" required>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label for="">Status:</label>
+                                        <select name="status" class="custom-select2 form-control"
+                                            style="width: 100%; height: 38px;" data-validation="required" required>
+                                            <option selected disabled value>Choose...</option>
+                                            <option value="active">Active</option>
+                                            <option value="inactive">Inactive</option>
+
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <button type="submit" class="btn btn-success btn-md float-right">
+                                            <i class="icon-copy dw dw-add"
+                                                style="font-family: dropways, Bangla526, sans-serif;"></i> Add
+                                            Bill Type
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
