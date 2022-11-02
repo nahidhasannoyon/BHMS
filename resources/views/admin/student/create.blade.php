84cfc00a8b1d4cf0f99adfcec838e8876a304238
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-02 16:39:48 +0600

    modifying the input fields  for admitting new student to hostel

--- resources/views/admin/student/create.blade.php
+++ resources/views/admin/student/create.blade.php
@@ -10,48 +10,55 @@
                 @csrf
                 <div class="col-md-12">
                     <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label>Student Name:</label><span class="text-danger">*</span>
-                            <input class="form-control" type="text" name="name" placeholder="Name" required
-                                value="{{ old('name') }}">
+                        <div class="form-group col-md-12">
+                            <label>Student Id - Name - Department :</label><span class="text-danger">*</span>
+                            <input class="form-control" type="text" name="id_name_dept" placeholder="Id - Name - Dept. "
+                                required value="">
                         </div>
-                        <div class="form-group col-md-6">
-                            <label>Student ID:</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="studentID" data-validation="required"
-                                value="{{ old('studentID') }}" required>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="">Department:</label><span class="text-danger">*</span>
-                            <select name="dept" class="custom-select2 form-control" style="width: 100%; height: 38px;"
-                                data-validation="required" required>
+
+                        <div class="form-group col-md-4">
+                            <label for="">Building Name:</label><span class="text-danger">*</span>
+                            <select class="custom-select2 form-control" style="width: 100%; height: 38px;"
+                                name="building_name" data-validation="required" placeholder='Select a Value' required>
                                 <option selected disabled value>Choose...</option>
-                                <option value="CSE">CSE</option>
-                                <option value="EEE">EEE</option>
-                                <option value="CE">CE</option>
-                                <option value="BBA">BBA</option>
-                                <option value="English">English</option>
-                                <option value="LLB">LLB</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="">Seat Number:</label><span class="text-danger">*</span>
-                            <select class="custom-select2 form-control" style="width: 100%; height: 38px;" name="seat_id"
-                                data-validation="required" placeholder='Select a Value' required>
-                                <option selected disabled value>Choose...</option>
-                                @foreach ($hostels as $hostel)
-                                    <option value="{{ $hostel->id }}">
-                                        {{ $hostel->building_name . '-' . $hostel->floor . '-' . $hostel->flat . '-' . $hostel->seat }}
+                                @foreach ($hostel_buildings as $hostel_building)
+                                    <option value="{{ $hostel_building->id }}">
+                                        {{ $hostel_building->name }}
                                     </option>
                                 @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="">Floor Number:</label><span class="text-danger">*</span>
+                            <select class="form-control" style="width: 100%; height: 38px;" name="building_name"
+                                data-validation="required" placeholder='Select a Value' required>
+                                <option selected disabled value>Choose...</option>
 
                             </select>
                         </div>
-                        <div class="form-group col-md-6">
+                        <div class="form-group col-md-4">
+                            <label for="">Flat Number:</label><span class="text-danger">*</span>
+                            <select class="form-control" style="width: 100%; height: 38px;" name="building_name"
+                                data-validation="required" placeholder='Select a Value' required>
+                                <option selected disabled value>Choose...</option>
+
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="">Seat Number:</label><span class="text-danger">*</span>
+                            <select class="form-control" style="width: 100%; height: 38px;" name="building_name"
+                                data-validation="required" placeholder='Select a Value' required>
+                                <option selected disabled value>Choose...</option>
+
+                            </select>
+                        </div>
+
+                        <div class="form-group col-md-4">
                             <label for="">Student Phone Number:</label><span class="text-danger">*</span>
                             <input type="text" class="form-control" name="phone" data-validation="required"
                                 value="" required>
                         </div>
-                        <div class="form-group col-md-6">
+                        <div class="form-group col-md-4">
                             <label for="">Guardian Phone Number:</label><span class="text-danger">*</span>
                             <input type="text" class="form-control" name="g_phone" data-validation="required"
                                 value="" required>
@@ -64,7 +71,7 @@
                         <div class="form-group col-md-6">
                             <label for="">Status:</label>
                             <input type="text" value="Active" class="form-control text-success" name="status"
-                                data-validation="required" required>
+                                data-validation="required" required disabled>
                         </div>
                         <div class="form-group col-md-12">
                             <button class="btn btn-success btn-md float-right " type="submit"><i
