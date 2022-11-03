32b601696574686067ce12cbee94f3ef0cacb3e3
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-04 01:14:31 +0600

    changing the building model name

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BuildingName extends Model
+class Building extends Model
 {
     use HasFactory;
-    protected $table = 'building_names';
+    protected $table = 'buildings';
     protected $guarded = ['id'];
 }
