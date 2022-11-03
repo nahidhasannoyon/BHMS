a546f431674c6498f65e4ed25ce1a0a84741f6bf
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-03 23:29:10 +0600

    adding model for building name, flat, floor, seat

--- app/Models/Flat.php
+++ app/Models/Flat.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HostelBuilding extends Model
+class Flat extends Model
 {
     use HasFactory;
-    protected $table = "hostel_buildings";
+    protected $table = 'flats';
     protected $guarded = ['id'];
 }
