fa54011b5352248c69c210531799188eada432f7
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-08 21:14:58 +0600

    adding student view file

---
+++ resources/views/admin/student/view.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layout.master')
+
+@section('content')
+    <div class="card-box pd-20 height-100-p mb-30">
+        <div class="row align-items-center">
+            <div class="col-md-12">
+                <h4 class="text-primary text-center p-3">Student Profile</h4>
+                <span class="user-icon">
+                    <img class="rounded-circle  mx-auto d-block" src="{{ asset('vendors/images/admin.png') }}" alt=""
+                        style="border:1px solid;" height="200" width="200">
+                </span>
+                <hr>
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <td>Student ID: </td>
+                            <td>{{ $student->student_id }}</td>
+                        </tr>
+                        <tr>
+                            <td>Student Name: </td>
+                            <td>{{ $student->name }}</td>
+                        </tr>
+                        <tr>
+                            <td>Department: </td>
+                            <td>{{ $student->dept }}</td>
+                        </tr>
+                        <tr>
+                            <td>Contact Number: </td>
+                            <td>{{ $student->phone }}</td>
+                        </tr>
+                        <tr>
+                            <td>Guardian Number: </td>
+                            <td>{{ $student->g_phone }}</td>
+                        </tr>
+                        <tr>
+                            <td>Hostel Building: </td>
+                            <td>{{ $student->building }}</td>
+                        </tr>
+                        <tr>
+                            <td>Hostel Floor: </td>
+                            <td>{{ $student->floor }}</td>
+                        </tr>
+                        <tr>
+                            <td>Hostel Flat: </td>
+                            <td>{{ $student->flat }}</td>
+                        </tr>
+                        <tr>
+                            <td>Hostel Seat: </td>
+                            <td>{{ $student->seat }}</td>
+                        </tr>
+                        <tr>
+                            <td>Status: </td>
+                            <td>{{ $student->status }}</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+@endsection
