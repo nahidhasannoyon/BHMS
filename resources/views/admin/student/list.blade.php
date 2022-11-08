9a2bc3de35d5e0949e4c730ea5430b90028d5830
Eftay Khyrul Alam <eftay.khyrul.alam.baiust.09@gmail.com>
2022-11-08 23:17:43 +0600

    chng row color and add edit route(editOption)

--- resources/views/admin/student/list.blade.php
+++ resources/views/admin/student/list.blade.php
@@ -18,33 +18,38 @@
                     </thead>
                     <tbody>
                         @foreach ($students as $student)
-                            @if ($student->status == 1)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $student->student_id }}</td>
-                                    <td>{{ $student->name }}</td>
-                                    <td>{{ $student->status }}</td>
-                                    <td>
-                                        {{-- todo add functionality in these buttons --}}
-                                        <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip"
-                                            data-placement="bottom" title="download-student-info"><i
-                                                class="icon-copy dw dw-download"></i></a>
-                                        {{-- {{ route('student-information.download',$student->id) }} --}}
-                                        <a href="{{ route('view_student', $student->id) }}" class="btn btn-success btn-sm"
-                                            data-toggle="tooltip" data-placement="bottom" title="View"><i
-                                                class="icon-copy fi-torsos-female-male"></i></a>
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $student->student_id }}</td>
+                                <td>{{ $student->name }}</td>
+                                @if ($student->status == 1)
+                                    <td class="text-success"> Active </td>
+                                @elseif($student->status == 2)
+                                    <td class="text-secondary"> Inactive </td>
+                                @elseif($student->status == 3)
+                                    <td class="text-warning"> Cancelled </td>
+                                @else
+                                    <td class="text-danger"> Left </td>
+                                @endif
+                                <td>
+                                    {{-- todo add functionality in these buttons --}}
+                                    <a href="#" class="btn btn-info btn-sm" data-toggle="tooltip"
+                                        data-placement="bottom" title="download-student-info"><i
+                                            class="icon-copy dw dw-download"></i></a>
+                                    {{-- {{ route('student-information.download',$student->id) }} --}}
+                                    <a href="{{ route('view_student', $student->id) }}" class="btn btn-success btn-sm"
+                                        data-toggle="tooltip" data-placement="bottom" title="View"><i
+                                            class="icon-copy fi-torsos-female-male"></i></a>
 
-                                        <a href="#" class="btn btn-warning btn-sm" data-toggle="tooltip"
-                                            data-placement="bottom" title="Edit"><i
-                                                class="icon-copy dw dw-edit-1"></i></a>
-                                        {{-- {{route('student-information.edit', $student->id)}} --}}
-                                        <a href="#" class="btn btn-danger btn-sm" data-toggle="tooltip"
-                                            data-placement="bottom" title="Delete"><i
-                                                class="icon-copy dw dw-trash1"></i></a>
-                                        {{-- {{ route('student-information.destroy',$student->id)}} --}}
-                                    </td>
-                                </tr>
-                            @endif
+                                    <a href="{{ route('edit_student', $student->id) }}" class="btn btn-warning btn-sm"
+                                        data-toggle="tooltip" data-placement="bottom" title="Edit"><i
+                                            class="icon-copy dw dw-edit-1"></i></a>
+                                    {{-- {{route('student-information.edit', $student->id)}} --}}
+                                    <a href="#" class="btn btn-danger btn-sm" data-toggle="tooltip"
+                                        data-placement="bottom" title="Delete"><i class="icon-copy dw dw-trash1"></i></a>
+                                    {{-- {{ route('student-information.destroy',$student->id)}} --}}
+                                </td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
