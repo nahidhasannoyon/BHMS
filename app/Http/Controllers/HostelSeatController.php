74ad7266f9af681da95517e12cc328553d84a5b6
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-14 02:34:03 +0600

    changing the function parameter name and adding more parameter, adding logic to check to not add or update to existing named building, floor, flat or seat and on deletion delete form database and also from the descendant data tables

--- app/Http/Controllers/HostelSeatController.php
+++ app/Http/Controllers/HostelSeatController.php
@@ -42,10 +42,10 @@ class HostelSeatController extends Controller
         }
     }
 
-    public function edit_building($id)
+    public function edit_building($building_id)
     {
         try {
-            $building = Building::where('id', $id)->first();
+            $building = Building::where('id', $building_id)->first();
             return view('admin.hostel.edit_building', compact('building'));
         } catch (\Throwable $th) {
             return $th->getMessage();
@@ -55,20 +55,44 @@ class HostelSeatController extends Controller
     public function update_building(Request $request)
     {
         try {
-            $building = Building::where('id', $request->id)->first();
-            $building->name = $request->name;
-            $building->save();
-            toast('Hostel Building updated.', 'success');
-            return redirect()->back();
+            $is_exist = Building::where('name', $request->name)->first();
+            if ($is_exist) {
+                toast('Building name already exist.', 'error');
+                return redirect()->back();
+            } else {
+                $building = Building::where('id', $request->id)->first();
+                $building->name = $request->name;
+                $building->save();
+                toast('Hostel Building updated.', 'success');
+                return redirect()->back();
+            }
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
     }
 
-    public function delete_building($id)
+    public function delete_building($building_id)
     {
         try {
-            $building = Building::where('id', $id)->first();
+            $seats = Seat::where('building_id', $building_id)->get();
+            if ($seats->count() > 0) {
+                foreach ($seats as $seat) {
+                    $seat->delete();
+                }
+            }
+            $flats = Flat::where('building_id', $building_id)->get();
+            if ($flats->count() > 0) {
+                foreach ($flats as $flat) {
+                    $flat->delete();
+                }
+            }
+            $floors = Floor::where('building_id', $building_id)->get();
+            if ($floors->count() > 0) {
+                foreach ($floors as $floor) {
+                    $floor->delete();
+                }
+            }
+            $building = Building::where('id', $building_id)->first();
             $building->delete();
             toast('Hostel Building deleted.', 'success');
             return redirect()->back();
@@ -90,21 +114,74 @@ class HostelSeatController extends Controller
         }
     }
 
-    public function add_floor(Request $request)
+    public function add_floor($building_id, Request $request)
     {
         try {
-            $is_exist = floor::where('name', $request->name)->first();
+            $is_exist = Floor::where('name', $request->name)->where('building_id', $building_id)->first();
             if ($is_exist) {
                 toast('Floor name already exist.', 'error');
                 return redirect()->back();
             } else {
                 $floor = new Floor();
                 $floor->name = $request->name;
-                $floor->building_id = $request->building_id;
+                $floor->building_id = $building_id;
                 $floor->save();
-                toast('New Floor added.', 'success');
+                toast('New Hostel Floor added.', 'success');
                 return redirect()->back();
             }
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function edit_floor($building_id, $floor_id)
+    {
+        try {
+            $floor = Floor::where('id', $floor_id)->first();
+            return view('admin.hostel.edit_floor', compact('floor'));
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function update_floor(Request $request)
+    {
+        try {
+            $is_exist = Floor::where('name', $request->name)->where('building_id', $request->building_id)->first();
+            if ($is_exist) {
+                toast('Floor name already exist.', 'error');
+                return redirect()->back();
+            } else {
+                $floor = Floor::where('id', $request->id)->first();
+                $floor->name = $request->name;
+                $floor->save();
+                toast('Floor updated.', 'success');
+                return redirect()->back();
+            }
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function delete_floor($building_id, $floor_id,)
+    {
+        try {
+            $seats = Seat::where('floor_id', $floor_id)->get();
+            if ($seats->count() > 0) {
+                foreach ($seats as $seat) {
+                    $seat->delete();
+                }
+            }
+            $flats = Flat::where('floor_id', $floor_id)->get();
+            if ($flats->count() > 0) {
+                foreach ($flats as $flat) {
+                    $flat->delete();
+                }
+            }
+            $floor = Floor::where('id', $floor_id)->first();
+            $floor->delete();
+            toast('Floor deleted.', 'success');
+            return redirect()->back();
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
@@ -126,7 +203,7 @@ class HostelSeatController extends Controller
     public function add_flat(Request $request)
     {
         try {
-            $is_exist = Flat::where('name', $request->name)->first();
+            $is_exist = Flat::where('building_id', $request->building_id)->where('floor_id', $request->floor_id)->where('name', $request->name)->exists();
             if ($is_exist) {
                 toast('Flat name already exist.', 'error');
                 return redirect()->back();
@@ -139,6 +216,53 @@ class HostelSeatController extends Controller
                 toast('New Flat added.', 'success');
                 return redirect()->back();
             }
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function edit_flat($building_id, $floor_id, $flat_id)
+    {
+        try {
+            $flat = Flat::where('id', $flat_id)->first();
+            return view('admin.hostel.edit_flat', compact('flat'));
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function update_flat(Request $request)
+    {
+        try {
+            $is_exist = Flat::where('building_id', $request->building_id)->where('floor_id', $request->floor_id)->where('name', $request->name)->exists();
+            if ($is_exist) {
+                toast('Flat name already exist.', 'error');
+                return redirect()->back();
+            } else {
+                $flat = Flat::where('id', $request->id)->first();
+                $flat->name = $request->name;
+                $flat->save();
+                toast('Flat updated.', 'success');
+                return redirect()->back();
+            }
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function delete_flat($building_id, $floor_id, $flat_id)
+    {
+        try {
+            $seats = Seat::where('flat_id', $flat_id)->get();
+            if ($seats->count() > 0) {
+                foreach ($seats as $seat) {
+                    $seat->delete();
+                }
+            }
+            $flat = Flat::where('id', $flat_id)->first();
+            $flat->delete();
+            toast('Flat deleted.', 'success');
+            return redirect()->back();
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
@@ -160,14 +284,14 @@ class HostelSeatController extends Controller
     public function add_seat(Request $request)
     {
         try {
-            $is_exist = Seat::where('name', $request->name)->first();
+            $is_exist = Seat::where('building_id', $request->building_id)->where('floor_id', $request->floor_id)->where('flat_id', $request->flat_id)->where('name', $request->name)->exists();
             if ($is_exist) {
                 toast('Seat name already exist.', 'error');
                 return redirect()->back();
             } else {
                 $seat = new Seat();
                 $seat->name = $request->name;
-                $seat->status = $request->status;
+                $seat->status = 0;
                 $seat->building_id = $request->building_id;
                 $seat->floor_id = $request->floor_id;
                 $seat->flat_id = $request->flat_id;
@@ -175,6 +299,47 @@ class HostelSeatController extends Controller
                 toast('New Seat added.', 'success');
                 return redirect()->back();
             }
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function edit_seat($building_id, $floor_id, $flat_id, $seat_id)
+    {
+        try {
+            $seat = Seat::where('id', $seat_id)->first();
+            return view('admin.hostel.edit_seat', compact('seat'));
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function update_seat(Request $request)
+    {
+        try {
+            $is_exist = Seat::where('building_id', $request->building_id)->where('floor_id', $request->floor_id)->where('flat_id', $request->flat_id)->where('name', $request->name)->exists();
+            if ($is_exist) {
+                toast('Seat name already exist.', 'error');
+                return redirect()->back();
+            } else {
+                $seat = Seat::where('id', $request->id)->first();
+                $seat->name = $request->name;
+                $seat->save();
+                toast('Seat updated.', 'success');
+                return redirect()->back();
+            }
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+
+    public function delete_seat($building_id, $floor_id, $flat_id, $seat_id)
+    {
+        try {
+            $seat = Seat::where('id', $seat_id)->first();
+            $seat->delete();
+            toast('Seat deleted.', 'success');
+            return redirect()->back();
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
