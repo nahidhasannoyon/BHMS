fbc7e4dcee91fec724385226648292b15e4f4b8f
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-11-14 02:30:29 +0600

    adding button to show building list page, and changing submit route

--- resources/views/admin/hostel/edit_building.blade.php
+++ resources/views/admin/hostel/edit_building.blade.php
@@ -1,11 +1,21 @@
 @extends('admin.layout.master')
 @section('content')
     <div class="pd-20 card-box mb-30">
+        <div class="row">
+            <div class="col-md-12"><a
+                    href="{{ route('admin.hostel.list', [
+                        'building' => $building->id,
+                    ]) }}"
+                    class="btn btn-primary btn-md float-left">
+                    <i class="icon-copy bi bi-arrow-90deg-left" style="font-family: dropways, Bangla526, sans-serif;"></i>
+                    Buildings</a>
+            </div>
+        </div>
         <div class="text-center">
             <h4 class="text-blue" style="padding-bottom: 10px"><u>Edit Building</u></h4>
         </div>
         <div class="col-md-12">
-            <form action="{{ route('admin.hostel.update_building', $building->id) }}" method="POST">
+            <form action="{{ route('admin.hostel.update', $building->id) }}" method="POST">
                 @csrf
                 <div class="col-md-12">
                     <div class="form-row">
@@ -14,6 +24,7 @@
                             <input type="text" class="form-control" name="name" required
                                 value="{{ $building->name }}">
                         </div>
+                        <input type="hidden" name="id" value="{{ $building->id }}">
                         <div class="form-group col-md-12">
                             <button type="submit" class="btn btn-success btn-md float-right">
                                 <i style="font-family: dropways, Bangla526, sans-serif;"></i>
