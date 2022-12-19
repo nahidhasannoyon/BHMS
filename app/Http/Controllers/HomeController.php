e14eaa12dfe5af64db96299c2ccc42ee504a1b22
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-19 22:57:13 +0600

    adding admin logout, profile and update profile function in controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Toaster;
+use Illuminate\Support\Facades\Hash;
+use GrahamCampbell\ResultType\Success;
 
 class HomeController extends Controller
 {
@@ -24,7 +27,46 @@ class HomeController extends Controller
     public function showAdminDashboard()
     {
         try {
+            // return auth()->user();
             return view("admin.layout.dashboard");
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function adminLogout()
+    {
+        try {
+            auth()->logout();
+            session()->flush();
+            toast('You have been logged out successfully', 'success');
+            return redirect()->route("admin.login_form");
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function adminProfile()
+    {
+        try {
+            return view("admin.layout.profile");
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+    public function updateProfile(Request $request)
+    {
+        try {
+            // return $request->all();
+            if (Hash::check($request->password, auth()->user()->password)) {
+                auth()->user()->update([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                ]);
+            } else {
+                toast('Password is incorrect', 'error');
+                return redirect()->back();
+            }
+            toast('Profile updated successfully', 'success');
+            return redirect()->back();
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
