04bdfacfb3d9841cca9d43da9adc1bc56faa3cde
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-21 00:32:07 +0600

    removing remember checkbox

--- resources/views/student/auth/login.blade.php
+++ resources/views/student/auth/login.blade.php
@@ -72,14 +72,8 @@
                                 </div>
                             </div>
                             <div class="row pb-30">
-                                <div class="col-6">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Remember</label>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="forgot-password"><a href="forgot-password.html">Forgot Password</a>
+                                <div class="col-12">
+                                    <div class="forgot-password"><a href="">Forgot Password</a>
                                     </div>
                                 </div>
                             </div>
