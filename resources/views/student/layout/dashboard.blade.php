c026d1c13d85ee6e653a252285818e639b460885
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-22 02:17:44 +0600

    adding welcome msg in student dashboard

--- resources/views/student/layout/dashboard.blade.php
+++ resources/views/student/layout/dashboard.blade.php
@@ -1,3 +1,20 @@
 @extends('student.layout.master')
 @section('content')
+    <div class="card-box pd-20 height-100-p mb-30">
+
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <img src="{{ asset('vendors/images/banner-img.png') }}" alt="">
+            </div>
+            <div class="col-md-8">
+                <h4 class="font-20 weight-500 mb-10 text-capitalize">
+                    Welcome back
+                    <div class="weight-600 font-30 text-blue">{{ auth('student')->user()->name }}!</div>
+                </h4>
+                <p class="font-18 max-width-600">
+                    Keep it up the good work. You are doing great.
+                </p>
+            </div>
+        </div>
+    </div>
 @endsection
